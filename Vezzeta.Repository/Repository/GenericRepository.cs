1fd7bf07a2ce0045ea969ca542b1834564e3bc32
IbrahimKaram <ahmedrmady0@gmail.com>
2024-08-06 10:32:05 +0300

    ..

--- Vezzeta.Repository/Repository/GenericRepository.cs
+++ Vezzeta.Repository/Repository/GenericRepository.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vezzeta.Core.Contracts.Repository.Contracts;
+using Vezzeta.Core.Data.Entities;
 
 namespace Vezzeta.Repository.Repository
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
